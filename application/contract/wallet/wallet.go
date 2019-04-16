acc117710fa9a6fa5063c4e89972941e6f6519ac
egor <egor.pol9kov@gmail.com>
2019-04-16 09:50:01 +0300

    WLT-297: removed allowance; changed wallet transfer.

--- application/contract/wallet/wallet.go
+++ application/contract/wallet/wallet.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 
 	"github.com/insolar/insolar/application/contract/wallet/safemath"
-	"github.com/insolar/insolar/application/proxy/allowance"
 	"github.com/insolar/insolar/application/proxy/wallet"
 	"github.com/insolar/insolar/insolar"
 	"github.com/insolar/insolar/logicrunner/goplugin/foundation"
@@ -40,34 +39,19 @@ func (w *Wallet) Transfer(amount uint, to *insolar.Reference) error {
 		return fmt.Errorf("[ Transfer ] Can't get implementation: %s", err.Error())
 	}
 
-	toWalletRef := toWallet.GetReference()
-
 	newBalance, err := safemath.Sub(w.Balance, amount)
 	if err != nil {
 		return fmt.Errorf("[ Transfer ] Not enough balance for transfer: %s", err.Error())
 	}
-
-	ah := allowance.New(&toWalletRef, amount, w.GetContext().Time.Unix()+10)
-	a, err := ah.AsChild(w.GetReference())
-	if err != nil {
-		return fmt.Errorf("[ Transfer ] Can't save as child: %s", err.Error())
-	}
-
-	// Changing balance only after allowance was successfully create
 	w.Balance = newBalance
 
-	r := a.GetReference()
-	err = toWallet.AcceptNoWait(&r)
+	err = toWallet.Accept(amount)
 	return err
 }
 
 // Accept transforms allowance to balance
-func (w *Wallet) Accept(aRef *insolar.Reference) error {
-	b, err := allowance.GetObject(*aRef).TakeAmount()
-	if err != nil {
-		return fmt.Errorf("[ Accept ] Can't take amount: %s", err.Error())
-	}
-	w.Balance, err = safemath.Add(w.Balance, b)
+func (w *Wallet) Accept(amount uint) (err error) {
+	w.Balance, err = safemath.Add(w.Balance, amount)
 	if err != nil {
 		return fmt.Errorf("[ Accept ] Couldn't add amount to balance: %s", err.Error())
 	}
@@ -76,32 +60,6 @@ func (w *Wallet) Accept(aRef *insolar.Reference) error {
 
 // GetBalance gets total balance
 func (w *Wallet) GetBalance() (uint, error) {
-	iterator, err := w.NewChildrenTypedIterator(allowance.GetPrototype())
-	if err != nil {
-		return 0, fmt.Errorf("[ GetBalance ] Can't get children: %s", err.Error())
-	}
-
-	for iterator.HasNext() {
-		cref, err := iterator.Next()
-		if err != nil {
-			return 0, fmt.Errorf("[ GetBalance ] Can't get next child: %s", err.Error())
-		}
-
-		if !cref.IsEmpty() {
-			a := allowance.GetObject(cref)
-			balance, err := a.GetExpiredBalance()
-
-			if err != nil {
-				balance = 0
-				//return 0, fmt.Errorf("[ GetBalance ] Can't get balance for owner: %s", err.Error())
-			}
-
-			w.Balance, err = safemath.Add(w.Balance, balance)
-			if err != nil {
-				return 0, fmt.Errorf("[ GetBalance ] Couldn't add expired allowance to balance: %s", err.Error())
-			}
-		}
-	}
 	return w.Balance, nil
 }
 
